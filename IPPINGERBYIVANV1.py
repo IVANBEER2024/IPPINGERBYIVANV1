2a258255a03d3ffb61db90a4d87e1809d715f905
IVANBEER2024 <160456977+IVANBEER2024@users.noreply.github.com>
2024-02-22 23:15:51 +0700

    Add files via upload

---
+++ IPPINGERBYIVANV1.py
@@ -0,0 +1,67 @@
+import colorama
+import os
+import time
+import ctypes
+ctypes.windll.kernel32.SetConsoleTitleW("FUCK YOU!!                                                                                                                           MadeByKojak")
+def ipping():
+    os.system("cls")
+    count = 1
+    print("                                                            /´¯/) ")
+    print("\033[1;35;40m")
+    print("                                                          ,/¯../ ")
+    print("\033[1;39;40m")
+    print("                                                         /..../ ")
+    print("\033[1;35;40m")
+    print("                                                  /´¯/'...'/´¯¯`·¸ ")
+    print("\033[1;39;40m")
+    print("                                               /'/.../..../......./¨¯\ ")
+    print("\033[1;35;40m")
+    print("                                             ('(...´...´.... ¯~/'...') ")
+    print("\033[1;39;40m")
+    print("                                             \.................'....  ")
+    print("\033[1;35;40m")
+    print("                                              ''...\.......... _.·´ ") 
+    print("\033[1;39;40m")                   
+    print("                                                \..............( ")
+    print("\033[1;35;40m")
+    print("                                                  \.............\ ")
+    print("\033[1;39;40m")
+    e = input("")
+    os.system("cls")
+    replies = 0
+    replies += 1
+    hostname = e
+    os.system("cls")
+    
+    print("                                                            /´¯/) ")
+    print("\033[1;35;40m")
+    print("                                                          ,/¯../ ")
+    print("\033[1;39;40m")
+    print("                                                         /..../ ")
+    print("\033[1;35;40m")
+    print("                                                  /´¯/'...'/´¯¯`·¸ ")
+    print("\033[1;39;40m")
+    print("                                               /'/.../..../......./¨¯\ ")
+    print("\033[1;35;40m")
+    print("                                             ('(...´...´.... ¯~/'...') ")
+    print("\033[1;39;40m")
+    print("                                             \.................'....  ")
+    print("\033[1;35;40m")
+    print("                                              ''...\.......... _.·´ ") 
+    print("\033[1;39;40m")                   
+    print("                                                \..............( ")
+    print("\033[1;35;40m")
+    print("                                                  \.............\ ")
+    while True:
+        response = os.system("ping -n 1 " + hostname + " >nul")
+        if response == 0:
+            print("\033[1;39;40m" + hostname + " ONLINE")
+            print("\033[1;35;40m" + hostname + " ONLINE")
+            print("\033[1;39;40m" + hostname + " ONLINE")
+        else:
+            print("\033[1;37;45m" + hostname + " IP SLAMED") 
+        count += 0
+        time.sleep(.001)
+
+
+ipping()
